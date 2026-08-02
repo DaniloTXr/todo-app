919c6124a920a0f04d00e41d78cc414b49487b2e
Danilo <danilo.teixa@gmail.com>
2026-08-02 16:36:34 -0300

    Add model/task and routes/tasks

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import taskRoutes from './routes/tasks.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use('/tasks', taskRoutes);
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log(`Connect to MongoDB`))
     .catch((err) => console.error(`Failed to connect to MongoDB:`, err));
 
 app.get('/', (req, res) => {
-    res.send('Server Up!')
+    res.send('Server up!')
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escutando na porta ${PORT}`);
+  console.log(`server listening on port ${PORT}`);
 });
