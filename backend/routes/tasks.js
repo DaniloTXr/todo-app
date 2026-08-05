5b96598f1f1595403b785affa91ba498f1c30fe0
Danilo <danilo.teixa@gmail.com>
2026-08-05 17:21:35 -0300

    Added routes POST, PATCH and DELETE, changed PATCH to use returnDocument because of deprecation of new:true

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -12,4 +12,49 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const newTask = new Task({
+      title: req.body.title,
+    });
+
+    const savedTask = await newTask.save();
+    res.status(201).json(savedTask);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { returnDocument: 'after' }
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ error: 'Tasks not found' });
+    }
+
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id);
+
+    if (!deletedTask) {
+      return res.status(404).json({ error: 'Tarefa não encontrada' });
+    }
+
+    res.json({ message: 'Tarefa removida com sucesso' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 export default router;
